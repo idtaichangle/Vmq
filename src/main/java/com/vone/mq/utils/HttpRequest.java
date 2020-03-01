cf7aa78e61b87ecbfc418d0f92fed37412de340b
lixy <lixianyi@live.com>
2020-03-01 14:13:11 +0800

    用apache httpclient发送http请求

--- src/main/java/com/vone/mq/utils/HttpRequest.java
+++ src/main/java/com/vone/mq/utils/HttpRequest.java
@@ -1,15 +1,71 @@
 package com.vone.mq.utils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.config.SocketConfig;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.cookie.ClientCookie;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.util.EntityUtils;
+
+import javax.net.ssl.SSLContext;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class HttpRequest {
+
+    static CloseableHttpClient httpclient = null;
+    static {
+
+        SSLContext sslContext = null;
+        try {
+            sslContext = new SSLContextBuilder().loadTrustMaterial(null, (arg0, arg1) -> true).build();
+        } catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
+            e.printStackTrace();
+        }
+        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext,
+                new NoopHostnameVerifier());
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("https", socketFactory).register("http", new PlainConnectionSocketFactory()).build();
+
+        SocketConfig socketConfig= SocketConfig.custom().setSoTimeout(30000).build();
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(
+                socketFactoryRegistry);
+        connectionManager.setDefaultSocketConfig(socketConfig);
+        connectionManager.setMaxTotal(200);
+        connectionManager.setDefaultMaxPerRoute(20);
+
+        httpclient = HttpClients.custom().setConnectionManager(connectionManager).build();
+
+    }
+
     /**
      * 向指定URL发送GET方法的请求
      *
@@ -21,48 +77,8 @@ public class HttpRequest {
      */
     public static String sendGet(String url, String param) {
         String result = "";
-        BufferedReader in = null;
-        try {
-            String urlNameString = url + "?" + param;
-            URL realUrl = new URL(urlNameString);
-            // 打开和URL之间的连接
-            URLConnection connection = realUrl.openConnection();
-            // 设置通用的请求属性
-            connection.setRequestProperty("accept", "*/*");
-            connection.setRequestProperty("connection", "Keep-Alive");
-            connection.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            // 建立实际的连接
-            connection.connect();
-            // 获取所有响应头字段
-            Map<String, List<String>> map = connection.getHeaderFields();
-            // 遍历所有的响应头字段
-            for (String key : map.keySet()) {
-                System.out.println(key + "--->" + map.get(key));
-            }
-            // 定义 BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(new InputStreamReader(
-                    connection.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result += line;
-            }
-        } catch (Exception e) {
-            System.out.println("发送GET请求出现异常！" + e);
-            e.printStackTrace();
-            result = "服务器无响应";
-        }
-        // 使用finally块来关闭输入流
-        finally {
-            try {
-                if (in != null) {
-                    in.close();
-                }
-            } catch (Exception e2) {
-                e2.printStackTrace();
-            }
-        }
-        return result;
+        HttpGet httpGet = new HttpGet(url+"?"+param);
+        return sendHttp(httpGet,null,null,10000);
     }
 
     /**
@@ -75,52 +91,70 @@ public class HttpRequest {
      * @return 所代表远程资源的响应结果
      */
     public static String sendPost(String url, String param) {
-        PrintWriter out = null;
-        BufferedReader in = null;
-        String result = "";
-        try {
-            URL realUrl = new URL(url);
-            // 打开和URL之间的连接
-            URLConnection conn = realUrl.openConnection();
-            // 设置通用的请求属性
-            conn.setRequestProperty("accept", "*/*");
-            conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            // 发送POST请求必须设置如下两行
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            // 获取URLConnection对象对应的输出流
-            out = new PrintWriter(conn.getOutputStream());
-            // 发送请求参数
-            out.print(param);
-            // flush输出流的缓冲
-            out.flush();
-            // 定义BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result += line;
+        HttpPost httpPost = new HttpPost(url);
+        if (param != null && param.length() > 0) {
+            List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+            for (String p : param.split("&")) {
+                String pair[] = p.split("=");
+                nvps.add(new BasicNameValuePair(pair[0], pair[1]));
+                try {
+                    httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+                } catch (UnsupportedEncodingException e1) {
+                    e1.printStackTrace();
+                }
             }
+        }
+
+        return sendHttp(httpPost,null,null,10000);
+    }
+
+
+    public static String sendHttp(HttpRequestBase requestMethod, HashMap<String, String> header,
+                                                 HashMap<String, String> cookie, int timeout) {
+
+        CloseableHttpResponse response1 = null;
+
+        if (header != null && header.size() > 0) {
+            for (Map.Entry<String, String> entry : header.entrySet()) {
+                requestMethod.setHeader(entry.getKey(), entry.getValue());
+            }
+        }
+
+        HttpClientContext context = HttpClientContext.create();
+        RequestConfig.Builder configBuilder = RequestConfig.custom();
+        configBuilder.setConnectionRequestTimeout(timeout);
+        configBuilder.setConnectTimeout(timeout);
+        configBuilder.setSocketTimeout(timeout);
+        context.setRequestConfig(configBuilder.build());
+
+        if (cookie != null && cookie.size() > 0) {
+            CookieStore cookieStore = new BasicCookieStore();
+            String domain = requestMethod.getURI().getAuthority();
+
+            if (domain.substring(domain.indexOf('.') + 1).contains(".")) {// 如果域名中只有一个点，就不要取子域名了。
+                domain = domain.substring(domain.indexOf('.'));
+            }
+
+            for (Map.Entry<String, String> entry : cookie.entrySet()) {
+                BasicClientCookie c = new BasicClientCookie(entry.getKey(), entry.getValue());
+                c.setPath("/");
+                c.setDomain(domain);
+                c.setAttribute(ClientCookie.DOMAIN_ATTR, "true");
+                cookieStore.addCookie(c);
+            }
+            context.setCookieStore(cookieStore);
+        }
+
+        try {
+            response1 = httpclient.execute(requestMethod, context);
+            HttpEntity entity = response1.getEntity();
+            String result = EntityUtils.toString(entity);
+            EntityUtils.consume(entity);
+            return result;
         } catch (Exception e) {
-            System.out.println("发送 POST 请求出现异常！"+e);
             e.printStackTrace();
         }
-        //使用finally块来关闭输出流、输入流
-        finally{
-            try{
-                if(out!=null){
-                    out.close();
-                }
-                if(in!=null){
-                    in.close();
-                }
-            }
-            catch(IOException ex){
-                ex.printStackTrace();
-            }
-        }
-        return result;
+        return "";
     }
+
 }
